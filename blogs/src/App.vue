8a13877af88f5358d149d2018ef70cca7aefe992
aui_tuthousend <mraihan286@gmail.com>
2024-08-13 18:16:49 +0700

    initial commit

---
+++ blogs/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+document.title = 'pue'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ blogs/src/views/Home.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+import {ref, watchEffect} from "vue";
+
+  const data = ref([]);
+
+  // watchEffect(async ()=> {
+  //   let response = await fetch(`http://127.0.0.1:8000/api/getPeriode/Teknik%20Elektro`);
+  //   data.value = await response.json();
+  // })
+
+</script>
+
+<template>
+  halo
+<!--  {{data}}-->
+</template>
+
+<style scoped>
+
+</style>
